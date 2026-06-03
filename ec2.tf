186439df8617d7fc30c844b443354f098fcb88bc
Rahul Chowdary Paladugu <165880530+rahul-paladugu@users.noreply.github.com>
2026-06-03 13:20:51 +1200

    Modify EC2 instance resource to add subnet ID
    Updated EC2 instance configuration to include subnet ID.

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,9 @@
-#Create instance required for the project by capturing ami-id dynamically using data query.
+#Create instance using this module for your project
 resource "aws_instance" "main" {
-  count         = length(var.components) #Mandatory
-  ami           = var.ami_id #Mandatory
-  vpc_security_group_ids = var.sg_ids #Mandatory
-  instance_type = var.instance_type #Mandatory
-  tags          = merge({Name = "${var.components[count.index]}-${local.common_name}"}, var.common_tags)
-  provisioner "local-exec" {
-  command = "echo The server's IP address is ${self.private_ip}"
-  on_failure = continue
-  }
+  count                = length(var.components) #Mandatory
+  ami                  = var.ami_id #Mandatory
+  subnet_id            = var.subnet_id
+  instance_type        = var.instance_type #Mandatory
+vpc_security_group_ids = var.sg_ids #Mandatory
+  tags                 = merge({Name = "${var.components[count.index]}-${local.common_name}"}, var.common_tags)
 }
