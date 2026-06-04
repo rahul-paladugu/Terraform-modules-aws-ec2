017d038d1fb45d93630a4841372cbf6cc89860af
Rahul Chowdary Paladugu <165880530+rahul-paladugu@users.noreply.github.com>
2026-06-05 11:10:21 +1200

    Add IAM instance profile to EC2 instance
    Added IAM instance profile to the EC2 instance resource.

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,10 @@
-#Create instance using this module for your project
+#Create an instance using this module for your project
 resource "aws_instance" "main" {
   count                = length(var.components) #Mandatory
   ami                  = var.ami_id #Mandatory
   subnet_id            = var.subnet_id
   instance_type        = var.instance_type #Mandatory
+  iam_instance_profile = var.iam_instance_profile
 vpc_security_group_ids = var.sg_ids #Mandatory
   tags                 = merge({Name = "${var.components[count.index]}-${local.common_name}"}, var.common_tags)
 }
